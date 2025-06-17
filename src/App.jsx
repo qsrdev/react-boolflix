4423025b9a3cdb66f8dc116c6d7b188e55441064
qsrdev <matteo.albertini747@gmail.com>
2025-06-17 15:44:40 +0200

    feat: card render done - minor fixes to the code - css cleared

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,27 @@ function App() {
 
   useEffect(() => {
     axios.get(`${apiUrl}?api_key=${apiKey}&query=${fakeQuery}`).then((res) => {
-      console.log(res.data);
+      console.log(res.data.results);
+      setMoovies(res.data.results);
     });
   }, []);
 
   return (
-    <>
-      <h1>Questa è una prova per foolflix</h1>
-    </>
+    <div className="row row-cols-1 row-cols-md-3 row-cols-lg-6 g-5">
+      {moovies.map((moovie) => (
+        <div key={moovie.id} className="card">
+          <img src={`https://image.tmdb.org/t/p/w342${moovie.poster_path}`} className="card-img-top object-fit-cover " alt={`Immagine di ${moovie.title}`} />
+          <div className="card-body">
+            <h5 className="card-title">{moovie.title ? moovie.title : "Titolo non disponibile"}</h5>
+            <small className="text-body-secondary">{`Also known as ${moovie.original_title}`}</small>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">{`Lingua: ${moovie.original_language}`}</li>
+            </ul>
+            <div className="card-footer">{`Voto ${moovie.vote_average}`}</div>
+          </div>
+        </div>
+      ))}
+    </div>
   );
 }
 
