c239ecf9181456e7e0ce3b5a5b05ccee9487c365
qsrdev <matteo.albertini747@gmail.com>
2025-06-17 12:35:45 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Questa è una prova per foolflix</h1>
+    </>
+  );
+}
+
+export default App;
